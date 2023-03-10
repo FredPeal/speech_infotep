10899876eb23dea389769da0affaf67789b83d88
FredPeal <frederickpeal@gmail.com>
2023-03-10 00:31:28 -0400

    feat: ask for microphone

--- speech.js
+++ speech.js
@@ -87,3 +87,17 @@ function talk(text) {
     p.innerText = text;
     texts.appendChild(p);
 }
+function getLocalStream() {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        window.localStream = stream;
+        window.localAudio.srcObject = stream;
+        window.localAudio.autoplay = true;
+      })
+      .catch((err) => {
+        console.error(`you got an error: ${err}`);
+      });
+}
+  
+getLocalStream();
